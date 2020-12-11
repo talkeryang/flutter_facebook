069739365cb5ce820d3c99e3abfed65cda13e87d
v7lin <v7lin@qq.com>
2020-12-11 20:27:14 +0800

    facebook applinks

---
+++ facebook_applinks/example/android/app/src/main/java/io/github/v7lin/facebook_applinks_example/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.v7lin.facebook_applinks_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ facebook_applinks/ios/Classes/FacebookApplinksPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FacebookApplinksPlugin : NSObject<FlutterPlugin>
+@end
