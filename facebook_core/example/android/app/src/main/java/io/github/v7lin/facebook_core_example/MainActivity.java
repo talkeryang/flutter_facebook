6585b305169a7cdef7083e330ed302ecabef228a
v7lin <v7lin@qq.com>
2020-12-11 20:24:04 +0800

    facebook core

---
+++ facebook_core/example/android/app/src/main/java/io/github/v7lin/facebook_core_example/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.v7lin.facebook_core_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ facebook_core/ios/Classes/FacebookCorePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FacebookCorePlugin : NSObject<FlutterPlugin>
+@end
