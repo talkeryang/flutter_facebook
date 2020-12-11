48db33052c91a25a6163befaf063b03f0d9d268e
v7lin <v7lin@qq.com>
2020-12-11 21:02:34 +0800

    facebook core

--- facebook_core/test/facebook_core_test.dart
+++ facebook_core/test/facebook_core_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('facebook_core');
+  const MethodChannel channel = MethodChannel('v7lin.github.io/facebook_core');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
