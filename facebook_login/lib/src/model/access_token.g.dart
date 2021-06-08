ec91ee040aa6ff38e7fcdce39e82218541e89f55
laxyang <514939946@qq.com>
2021-06-08 20:09:22 +0800

    fix model AccessToken

--- facebook_login/lib/src/model/access_token.g.dart
+++ facebook_login/lib/src/model/access_token.g.dart
@@ -9,16 +9,16 @@ part of 'access_token.dart';
 AccessToken _$AccessTokenFromJson(Map<String, dynamic> json) {
   return AccessToken(
     token: json['token'] as String,
-    userId: json['user_id'] as String,
+    userId: json['userId'] as String,
     expires: json['expires'] as int,
-    applicationId: json['application_id'] as String,
-    lastRefresh: json['last_refresh'] as int,
-    graphDomain: json['graph_domain'] as String,
-    isExpired: json['is_expired'] as bool,
-    grantedPermissions: (json['granted_permissions'] as List<dynamic>?)
+    applicationId: json['applicationId'] as String,
+    lastRefresh: json['lastRefresh'] as int,
+    graphDomain: json['graphDomain'] as String,
+    isExpired: json['isExpired'] as bool,
+    grantedPermissions: (json['grantedPermissions'] as List<dynamic>?)
         ?.map((e) => e as String)
         .toList(),
-    declinedPermissions: (json['declined_permissions'] as List<dynamic>?)
+    declinedPermissions: (json['declinedPermissions'] as List<dynamic>?)
         ?.map((e) => e as String)
         .toList(),
   );
@@ -27,12 +27,12 @@ AccessToken _$AccessTokenFromJson(Map<String, dynamic> json) {
 Map<String, dynamic> _$AccessTokenToJson(AccessToken instance) =>
     <String, dynamic>{
       'token': instance.token,
-      'user_id': instance.userId,
+      'userId': instance.userId,
       'expires': instance.expires,
-      'application_id': instance.applicationId,
-      'last_refresh': instance.lastRefresh,
-      'graph_domain': instance.graphDomain,
-      'is_expired': instance.isExpired,
-      'granted_permissions': instance.grantedPermissions,
-      'declined_permissions': instance.declinedPermissions,
+      'applicationId': instance.applicationId,
+      'lastRefresh': instance.lastRefresh,
+      'graphDomain': instance.graphDomain,
+      'isExpired': instance.isExpired,
+      'grantedPermissions': instance.grantedPermissions,
+      'declinedPermissions': instance.declinedPermissions,
     };
