9405d961280012a735ce766225be4ead60f2e2ae
连书敏 <3073303@qq.com>
2021-06-24 15:50:21 +0800

    version

--- facebook_share/test/facebook_share_test.dart
+++ facebook_share/test/facebook_share_test.dart
@@ -3,21 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:facebook_share/facebook_share.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('facebook_share');
+  const MethodChannel channel = MethodChannel('com.zhangzhongyun/facebook_share');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {});
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await FacebookShare.platformVersion, '42');
   });
 }
